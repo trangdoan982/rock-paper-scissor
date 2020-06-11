074ea45a2cde636226effcda53f2f0c0d7b126f9
Trang Doan <44855874+trangdoan982@users.noreply.github.com>
2020-06-11 16:43:42 +0700

    Update rock_paper_scissor.js

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,50 +1,46 @@
-let playOneRound;
 
 //function that play a round of RPS
 function playOneRound() {
     let playerSelection = prompt('What move do you want to make?');
     playerSelection = playerSelection.toUpperCase();
-    if (playerSelection == 'ROCK' || playerSelection == 'PAPER' || playerSelection == 'SCISSOR') {
+    if ((playerSelection == 'ROCK') || (playerSelection == 'PAPER') || (playerSelection == 'SCISSOR')) {
         console.log('User entered right input');
-            break;
     }
     else {
-            playerSelection = prompt('Please enter rock, or paper, or scissor');
-            break;
-            console.log('Wrong input from user');
+            playerSelection = prompt('Please enter rock, or paper, or scissor').toUpperCase();
+            console.log('Wrong input from user'); 
     }
 
-    console.log(playerSelection);
 
-    computerSelection = computerPlay()
+    let computerSelection = computerPlay();
 
     //if playerSelection == computerSelection then there's a draw
-    if playerSelection == computerSelection {
+    if (playerSelection.toLowerCase() == computerSelection) {
         console.log('Draw');
         return 'Draw';
     }
     //configure win-lose situations
-    else if (playerSelection == 'ROCK' && computerSelection == 'paper' {
+    else if ((playerSelection == 'ROCK') && (computerSelection == 'paper')) {
         console.log('computer wins');
         return "Computer wins";
     }
-    else if (playerSelection == 'ROCK' && computerSelection == 'scissor' {
+    else if ((playerSelection == 'ROCK') && (computerSelection == 'scissor')) {
         console.log('Player wins');
         return "Player wins";
     }
-    else if (playerSelection == 'PAPER' && computerSelection == 'rock' {
+    else if ((playerSelection == 'PAPER') && (computerSelection == 'rock')) {
         console.log('Player wins');
         return "Player wins";
     }
-    else if (playerSelection == 'PAPER' && computerSelection == 'scissor' {
+    else if ((playerSelection == 'PAPER') && (computerSelection == 'scissor')) {
         console.log('computer wins');
         return "Computer wins";
     }
-    else if (playerSelection == 'SCISSOR' && computerSelection == 'paper' {
+    else if ((playerSelection == 'SCISSOR') && (computerSelection == 'paper')) {
         console.log('Player wins');
         return "Player wins";
     }
-    else if (playerSelection == 'SCISSOR' && computerSelection == 'rock' {
+    else if ((playerSelection == 'SCISSOR') && (computerSelection == 'rock')) {
         console.log('computer wins');
         return "Computer wins";
     }
@@ -55,8 +51,5 @@ function playOneRound() {
 //randomly generate computer's move
 function computerPlay() {
     let moves=['rock', 'paper', 'scissors'];
-    return moves[Math.floor(Math.random() * moves.length)]
+    return moves[Math.floor(Math.random() * moves.length)];
 }
-console.log('Computer plays', computerPlay());
-
-playOneRound();
